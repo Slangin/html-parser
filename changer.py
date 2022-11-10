9a759d3f3057865b14232deca0d0d5b14b0537a6
Slangin <nevan.beal@gmail.com>
2022-11-10 09:28:21 -0500

    Finished script, was overcomplicating things

--- changer.py
+++ changer.py
@@ -1,7 +1,4 @@
 from bs4 import BeautifulSoup
-import urllib.request
-import urllib
-import re
 import sys
 
 
@@ -12,22 +9,29 @@ clean_links = []
 test = []
 
 soup = BeautifulSoup(open(html_page),'html.parser')
+images = soup.findAll('img')
+links = soup.findAll('a')
+link_1 = soup.findAll('link')
+script = soup.findAll('script')
 
-for link in soup.findAll("a", href=True):
-    cleaner = link['href']
-    clean_links.append(cleaner)
-
-
-for hl in clean_links:
-    urllib.parse.unquote(hl)
-    test.append(hl)
-
-
-
-print(clean_links)
-print(test)
-
+#Loop through alt image tags and santize paths
+for image in images:
+    image['src'] = image['src'].replace(' ', '-').replace('%20', '-').lower()
     
-
-
-
+#Loop through link tags, ignore #, and sanitize paths
+for link in links:
+    if link['href'][0] != '#':
+        link['href'] = link['href'].replace(' ', '-').replace('%20', '-').lower()
+        
+#Loop through script tags and sanitize path references       
+for sc in script:
+    sc['src'] = sc['src'].replace(' ', '-').replace('%20', '-').lower()
+   
+#Loop through link tags, ignore #, sanitize path references
+for lk in link_1:
+    if lk['href'][0] != '#':
+        lk['href'] = lk['href'].replace(' ', '-').replace('%20', '-').lower()
+        
+#open or create a new file and update the path information
+with open(sys.argv[2], 'w') as file:
+    file.write((soup.prettify()))
